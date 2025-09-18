b06a59138ad1ccbf29d5f24224ab248c9b13acf6
Guilherme Matsumura <guilhermepenamatsumura@gmail.com>
2025-09-18 18:37:02 -0300

    Criando metodos para notificar no console e injetando dependências

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService atividacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService atividacaoClienteService) {
+        this.atividacaoClienteService = atividacaoClienteService;
+
+        System.out.println("MeuPrimeiroController: " + atividacaoClienteService);
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
+        Cliente joao = new Cliente("João", "email@e.com", "21999999999");
+
+        atividacaoClienteService.ativar(joao);
+
         return "Hellozada";
     }
 }
